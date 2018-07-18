11a9d5fd01f7c390b984d5ae59114d195d2a0e6b
abskmj <abskmj@gmail.com>
2018-07-18 14:37:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL Developer Password Decryptor
+Oracle SQL Developer allows a user to associate passwords with connections so that the user doesn't have to enter the password each time he opens a connection. Of course, these passwords need to be stored somewhere. SQL developer stores them in an encrypted form, but it is possible to decrypt them.
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { enc, MD5, DES, mode, pad } from 'crypto-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  encryptedPassword = 'F35q3vdbVrI=0';
+  systemId = '3e8efb59-8a5a-4c13-b1d5-ff64f987787f';
+
+  password;
+
+  decrypt() {
+    try {
+      this.password = '';
+      let salt = "051399429372e8ad";
+
+      let keyWrap = enc.Hex.parse(enc.Utf8.parse(this.systemId).toString(enc.Hex) + salt);
+
+      for (let i = 0; i < 42; i++) {
+        keyWrap = MD5(keyWrap);
+      }
+
+      keyWrap = keyWrap.toString(enc.Hex);
+
+      let key = enc.Hex.parse(keyWrap.substr(0, 16));
+      let iv = enc.Hex.parse(keyWrap.substr(16, 16));
+      let data = enc.Base64.parse(this.encryptedPassword);
+
+      let decrypted = DES.decrypt({
+        ciphertext: data
+      }, key, {
+          iv: iv,
+          mode: mode.CBC,
+          padding: pad.AnsiX923
+        });
+
+      this.password = decrypted.toString(enc.Utf8);
+    }
+    catch (e) {
+      this.password = '';
+    }
+  }
+}
